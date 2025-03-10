58d5de07960cc807f29b83fcf9e106118be67426
Sfilosha <sfilosha@gmail.com>
2025-03-10 21:27:45 +0200

    persistor issue fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,6 +4,14 @@ import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "contacts",
@@ -20,6 +28,12 @@ const rootReducer = {
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
